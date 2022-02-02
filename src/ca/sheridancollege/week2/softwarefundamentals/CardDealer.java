5d5a59ae3ed845fc9963d89c3da7465435ab18b9
JingxinJiang <jiajingx@sheridancollege.ca>
2022-02-02 09:37:20 -0500

    Add a Card class

--- src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Modifier: Jingxin Jiang
+ * Student Number: 991657051
+ * 
  */
 package ca.sheridancollege.week2.softwarefundamentals;
 
@@ -15,5 +15,7 @@ public class CardDealer
     {
         Card nineClubs = new Card("Clbs", 9);
         nineClubs.setSuit("diamonds");
+        Card newClubs = new Card("cat",8);
+        newClubs.setSuit("Spades");
     }
 }
